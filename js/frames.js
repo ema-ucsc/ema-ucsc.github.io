841dc62aa94d27c5660da176fccc747ac8829a8e
ema-ucsc <94658691+ema-ucsc@users.noreply.github.com>
2021-11-18 22:33:19 -0300

    Add files via upload

---
+++ js/frames.js
@@ -0,0 +1,33 @@
+   
+function frames(variable){
+        document.getElementById("enframe").src="pages/home.html";
+        document.getElementById("enframe").height="550";
+    if (variable==1){
+        document.getElementById("enframe").src="pages/home.html";
+        document.getElementById("enframe").height="550";
+    }
+    else if (variable==2){
+        document.getElementById("enframe").src="pages/programa.html";
+        document.getElementById("enframe").height="550";
+    }
+    else if (variable==3){
+        document.getElementById("enframe").src="pages/cursillos.html";
+        document.getElementById("enframe").height="550";
+    }
+    else if (variable==4){
+        document.getElementById("enframe").src="pages/charlas.html";
+        document.getElementById("enframe").height="1280";
+    }   
+    else if (variable==5){
+        document.getElementById("enframe").src="pages/postulacion.html";
+        document.getElementById("enframe").height="550";
+    }
+    else if (variable==6){
+        document.getElementById("enframe").src="pages/patrocinadores.html";
+        document.getElementById("enframe").height="550";
+    }
+    else if (variable==7){
+        document.getElementById("enframe").src="pages/contacto.html";  
+        document.getElementById("enframe").height="550";
+    }
+} 
