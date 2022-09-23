faa2134a632fbcad65efbf4ad8856004a23b7c39
ema-ucsc <94658691+ema-ucsc@users.noreply.github.com>
2022-09-23 11:03:34 -0300

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -1,18 +1,18 @@
    
 function frames(variable){
         document.getElementById("enframe").src="pages/home.html";
-        document.getElementById("enframe").height="530";
+        document.getElementById("enframe").height="540";
     if (variable==1){
         document.getElementById("enframe").src="pages/home.html";
-        document.getElementById("enframe").height="530";
+        document.getElementById("enframe").height="540";
     }
     else if (variable==2){
         document.getElementById("enframe").src="pages/programa.html";
-        document.getElementById("enframe").height="530";
+        document.getElementById("enframe").height="540";
     }
     else if (variable==3){
         document.getElementById("enframe").src="pages/cursillos.html";
-        document.getElementById("enframe").height="530";
+        document.getElementById("enframe").height="540";
     }
     else if (variable==4){
         document.getElementById("enframe").src="pages/charlas.html";
@@ -24,14 +24,14 @@ function frames(variable){
     }
     else if (variable==6){
         document.getElementById("enframe").src="pages/patrocinadores.html";
-        document.getElementById("enframe").height="530";
+        document.getElementById("enframe").height="540";
     }
     else if (variable==7){
         document.getElementById("enframe").src="pages/galeria.html";  
-        document.getElementById("enframe").height="530";
+        document.getElementById("enframe").height="540";
     }   
     else if (variable==8){
         document.getElementById("enframe").src="pages/contacto.html";  
-        document.getElementById("enframe").height="530";
+        document.getElementById("enframe").height="540";
     }
 } 
