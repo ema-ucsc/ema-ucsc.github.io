264fedb06910a8ebc4afcfaf348c333206f3e5b7
ema-ucsc <94658691+ema-ucsc@users.noreply.github.com>
2025-08-18 22:41:34 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -36,6 +36,6 @@ function frames(variable){
     }
     else if (variable==9){
         document.getElementById("enframe").src="pages/becas.html";  
-        document.getElementById("enframe").height="1150";
+        document.getElementById("enframe").height="1250";
     }
 } 
