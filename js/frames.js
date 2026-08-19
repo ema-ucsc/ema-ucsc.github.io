64aee46dd6d42ece22559191e4d03881e9de9802
ema-ucsc <94658691+ema-ucsc@users.noreply.github.com>
2026-08-19 12:07:37 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -16,7 +16,7 @@ function frames(variable){
     }
     else if (variable==4){
         document.getElementById("enframe").src="pages/charlas.html";
-        document.getElementById("enframe").height="1310";
+        document.getElementById("enframe").height="1400";
     }   
     else if (variable==5){
         document.getElementById("enframe").src="pages/postulacion.html";
